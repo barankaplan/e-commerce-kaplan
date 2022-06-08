9d82d37d16af1cd29625c6bddbd71b7ab6187af3
Baran Kaplan <info@barankaplan.com>
2022-06-08 20:04:40 +0300

    logging

--- com.web.parent/backend/src/main/java/backend/admin/FileUploadUtil.java
+++ com.web.parent/backend/src/main/java/backend/admin/FileUploadUtil.java
@@ -1,5 +1,7 @@
 package backend.admin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -8,43 +10,46 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
 
 public class FileUploadUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+
     public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
 
         Path uploadPath = Paths.get(uploadDir);
 
-        if (!Files.exists(uploadPath)){
+        if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
         }
-        try(InputStream inputStream=multipartFile.getInputStream()){
-            Path filePath= uploadPath.resolve(fileName);
-            Files.copy(inputStream,filePath, StandardCopyOption.REPLACE_EXISTING);
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
 
-        }catch (IOException ex){
-            throw new IOException("Could not save file: "+fileName,ex);
+        } catch (IOException ex) {
+            throw new IOException("Could not save file: " + fileName, ex);
         }
     }
-    public static void cleanDir(String dir){
+
+    public static void cleanDir(String dir) {
         Path dirPath = Paths.get(dir);
 
-        try{
-            Files.list(dirPath).forEach(
-                    file-> {
+        try (Stream<Path> list = Files.list(dirPath);) {
+            list.forEach(
+                    file -> {
                         if (!Files.isDirectory(file)) {
                             try {
                                 Files.delete(file);
                             } catch (IOException e) {
-                                System.out.println("couldn't delete the file " +file);
+                                LOGGER.error("couldn't delete the file " + file);
                             }
                         }
                     }
             );
-        }catch (IOException ioException){
-            System.out.println("couldn't list directory!");
+        } catch (IOException ioException) {
+            LOGGER.error("couldn't list directory!");
         }
     }
-
 
 
 }
