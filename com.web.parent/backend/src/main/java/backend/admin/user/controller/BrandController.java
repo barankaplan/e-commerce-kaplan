2e16dc2fd3c2de0f499b4010d0726962123c7508
Baran Kaplan <info@barankaplan.com>
2022-06-15 04:41:32 +0300

    brands

--- com.web.parent/backend/src/main/java/backend/admin/user/controller/BrandController.java
+++ com.web.parent/backend/src/main/java/backend/admin/user/controller/BrandController.java
@@ -2,7 +2,9 @@ package backend.admin.user.controller;
 
 
 import backend.admin.user.service.BrandService;
+import backend.admin.user.service.CategoryService;
 import common.data.entity.Brand;
+import common.data.entity.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +15,11 @@ import java.util.List;
 public class BrandController {
 
     private final BrandService brandService;
+    private final CategoryService categoryService;
 
-    public BrandController(BrandService brandService) {
+    public BrandController(BrandService brandService, CategoryService categoryService) {
         this.brandService = brandService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping("/brands")
@@ -23,5 +27,16 @@ public class BrandController {
        List<Brand> listBrands= brandService.listAll();
        model.addAttribute("listBrands",listBrands);
        return "brands/brands";
+    }
+
+    @GetMapping("/brands/new")
+    public String newBrand(Model model) {
+        List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+
+        model.addAttribute("listCategories", listCategories);
+        model.addAttribute("brand", new Brand());
+        model.addAttribute("pageTitle", "Create New Brand");
+
+        return "brands/brand_form";
     }
 }
