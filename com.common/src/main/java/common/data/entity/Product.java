b7c9d80f5361e2e573936bf20836894b0595acfd
Baran Kaplan <info@barankaplan.com>
2022-06-17 00:12:38 +0300

    Create Product.java

---
+++ com.common/src/main/java/common/data/entity/Product.java
@@ -0,0 +1,70 @@
+package common.data.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 256, nullable = false)
+    private String name;
+
+    @Column(unique = true, length = 256, nullable = false)
+    private String alias;
+
+    @Column(length = 512, nullable = false, name = "short_description")
+    private String shortDescription;
+
+    @Column(length = 4096, nullable = false, name = "full_description")
+    private String fullDescription;
+
+    @Column(name = "created_time")
+    private Date createdTime;
+
+    @Column(name = "updated_time")
+    private Date updatedTime;
+
+    private boolean enabled;
+
+    @Column(name = "in_stock")
+    private boolean inStock;
+
+    private float cost;
+
+    private float price;
+
+    @Column(name = "discount_percent")
+    private float discountPercent;
+
+    private float length;
+    private float width;
+    private float height;
+    private float weight;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+	
+}
